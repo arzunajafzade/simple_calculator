249822ced5689584e1a22c2355176d769927fcb3
Arzu Najafzade <115610763+arzunajafzade@users.noreply.github.com>
2025-06-20 14:04:09 -0700

    Add files via upload
    add calculator script

---
+++ calculator.py
@@ -0,0 +1,14 @@
+b=float(input())
+a=input()
+c=float(input())
+if a=="+":
+    print(b+c)
+elif a=="-":
+    print(b-c)
+elif a=="*"   :
+    print(b*c)
+elif a=="/"   :
+    if c!=0:
+      print(b/c)
+
+
